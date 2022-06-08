826bca82528ad1f5725649b50e235276eb594a97
Francisco Cortez <franciscocortezdev@gmail.com>
2022-06-08 15:17:29 -0600

    Reestructuring files

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './Pages/Home/Home'
 import { Header } from './Components/Header/Header'
-
+import { Details } from './Pages/Details/Details'
 export default function App () {
   return (
     <>
     <Header/>
     <Routes>
     <Route path="/" element={<Home />} />
-
+    <Route path="/:id" element={<Details />} />
     </Routes>
 
     </>
