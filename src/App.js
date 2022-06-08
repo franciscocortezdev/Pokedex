98fa3bc6d68e83eb06c2cf2cec20b522e605cfab
Francisco Cortez <franciscocortezdev@gmail.com>
2022-06-08 15:06:52 -0600

    React rounting add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import { Home } from './Pages/Home/Home'
 import { Header } from './Components/Header/Header'
 
 export default function App () {
   return (
-
     <>
-      <Header/>
-      <Home />
+    <Header/>
+    <Routes>
+    <Route path="/" element={<Home />} />
+
+    </Routes>
+
     </>
 
   )
