fadfc4a9d8c344cc86fc38daad1d2c884cf4d4a8
Francisco Cortez <franciscocortezdev@gmail.com>
2022-06-14 21:04:19 -0600

    CustomHook IntersectionObserver

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { getAllPokemon } from '../../Service/getPokemon'
 import { CardPokemon } from '../../Components/CardPokemon/CardPokemon'
 import './Home.css'
 import { BtnUpPage } from '../../Components/BtnUpPage/BtnUpPage'
 import { Spinner } from '../../Components/Spinner/Spinner'
+import { useObserver } from '../../Hooks/useObserver'
 
 export function Home () {
   const [listPoke, setlistPoke] = useState([])
   const [offset, setOffset] = useState(0)
   const [loading, setLoading] = useState(true)
-  const [visible, setVisible] = useState(false)
-  const elObserver = useRef()
+  const [observed, elementRef] = useObserver()
 
   useEffect(() => {
     setLoading(true)
@@ -21,20 +21,10 @@ export function Home () {
   }, [offset])
 
   useEffect(() => {
-    const observer = new IntersectionObserver(newPokemons, {
-      root: null,
-      rootMargin: '5px',
-      threshold: 0.5
-    })
-    observer.observe(elObserver.current)
-    if (visible) {
+    if (observed) {
       setOffset(prevOffset => prevOffset + 8)
     }
-  }, [visible])
-
-  const newPokemons = (entries) => {
-    setVisible(entries[0].isIntersecting)
-  }
+  }, [observed])
 
   return (
     <>
@@ -55,7 +45,7 @@ export function Home () {
     <BtnUpPage/>
     {loading && <Spinner/>}
 
-    <div ref={elObserver}></div>
+    <div ref={elementRef}></div>
 
     </>
   )
