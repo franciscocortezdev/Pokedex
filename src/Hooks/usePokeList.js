5a61d4692ce9f1a4bc30bba34798fc42fabe8b8d
Francisco Cortez <franciscocortezdev@gmail.com>
2022-06-17 20:01:58 -0600

    Fixed fetch usePokeList

--- src/Hooks/usePokeList.js
+++ src/Hooks/usePokeList.js
@@ -7,25 +7,12 @@ export function usePokeList () {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    let offsetToUse
     setLoading(true)
 
-    if (offset === null) {
-      if (localStorage.getItem('offset') === null) {
-        offsetToUse = 0
-      } else {
-        offsetToUse = localStorage.getItem('offset')
-      }
-    } else {
-      offsetToUse = offset
-    }
-    console.log(localStorage.getItem('offset'))
-    console.log('of', offsetToUse)
-    getAllPokemon(offsetToUse).then(data => Promise.all(data).then(dat => {
+    getAllPokemon(offset).then(data => Promise.all(data).then(dat => {
       setlistPoke(prevPoke => [...prevPoke, ...dat])
 
       setLoading(false)
-      localStorage.setItem('offset', offsetToUse)
     }))
   }, [offset])
 
