8ce1bd25c479546f76e51e3d2a4d9c6197235ac3
Francisco Cortez <franciscocortezdev@gmail.com>
2022-06-18 16:39:54 -0600

    btnHome on page NotFound

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -2,12 +2,18 @@ import React from 'react'
 import './NotFound.css'
 import NotFound from '../../Img/NotFound.png'
 import { CardPokemon } from '../../Components/CardPokemon/CardPokemon'
+import { useNavigate } from 'react-router-dom'
 
 export function PokemonNFound () {
+  const navigate = useNavigate()
+  const handleHome = () => {
+    navigate('/Pokedex/')
+  }
   return (
     <>
     <div className='NotFoundContainer'>
     <CardPokemon Name={'Pokemon Not Found'} Image={NotFound}/>
+    <button className='btnHome' onClick={handleHome}>Go to Home</button>
     </div>
     </>
   )
